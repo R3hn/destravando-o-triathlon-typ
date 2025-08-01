2d248822277b7775e1cba4faca0e6ab9f8165f79
Bernardo Giovanni Covello Rehn <bernardo.c.rehn@gmail.com>
2025-08-01 02:04:44 +0000

    Tirar mais o espaçamento entre titulo e os blocos de entrar no grupo vip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-dvh bg-background font-body">
       <main className="flex-1">
-        <section className="w-full py-20 md:py-28 lg:py-32">
+        <section className="w-full py-12 md:py-16 lg:py-20">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
